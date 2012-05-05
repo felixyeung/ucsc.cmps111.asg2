250f868813ee00945ca46307e1c7043ce602a294
Matthew Menzel Musselman <mmusselm@unix3.ic.ucsc.edu>
2012-05-05 15:00:39 -0700

    No parameters passed yet.

--- usr/src/include/semaphore.h
+++ usr/src/include/semaphore.h
@@ -1,21 +1,31 @@
 #include <lib.h>
 
 PUBLIC int seminit(int sem, int value) {
+	message m;
 
+	_syscall(PM_PROC_NR, 103, &m);
 }
 
 PUBLIC int semvalue(int sem) {
+	message m;
 
+	_syscall(PM_PROC_NR, 105, &m);
 }
 
 PUBLIC int semup(int sem) {
+	message m;
 
+	_syscall(PM_PROC_NR, 108, &m);
 }
 
 PUBLIC int semdown(int sem) {
+	message m;
 
+	_syscall(PM_PROC_NR, 109, &m);
 }
 
 PUBLIC int semfree(int sem) {
+	message m;
 
+	_syscall(PM_PROC_NR, 110, &m);
 }
