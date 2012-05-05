eeef38dc7ac1aeb8123a99524e3f0cd70be23bc5
Matthew Menzel Musselman <mmusselm@unix3.ic.ucsc.edu>
2012-05-05 14:51:58 -0700

    Finally found: our user library for semaphores

---
+++ usr/src/include/semaphore.h
@@ -0,0 +1,21 @@
+#include <lib.h>
+
+PUBLIC int seminit(int sem, int value) {
+
+}
+
+PUBLIC int semvalue(int sem) {
+
+}
+
+PUBLIC int semup(int sem) {
+
+}
+
+PUBLIC int semdown(int sem) {
+
+}
+
+PUBLIC int semfree(int sem) {
+
+}
