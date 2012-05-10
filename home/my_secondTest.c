48a109d9777680a52833b9ac1e37284eb762b39f
unknown <Felix@Felix-PC.(none)>
2012-05-09 21:04:18 -0700

    test: seminit, semvalue, semfree, semvalue again. Working.

--- home/my_secondTest.c
+++ home/my_secondTest.c
@@ -4,12 +4,22 @@
 #include <errno.h>
 
 int main() {
-	int i;
+	int result;
 	int e;
 	message m;
 	e = errno;
-	seminit(2000, -100);
-	i = semvalue(2000);
-	printf("Test result: i=%d & e=%d\n", i, e);
+	
+	result = seminit(2000, -1);
+	printf("Create semaphore: result=%d & e=%d\n", result, e);
+	
+	result = semvalue(2000);
+	printf("semaphore value of the thing we just created: result=%d & e=%d\n", result, e);
+	
+	result = semfree(2000);
+	printf("Free semaphore (return 1 if successful): result=%d & e=%d\n", result, e);
+	
+	result = semvalue(2000);
+	printf("Can we still see value? this should fail: result=%d & e=%d\n", result, e);
+	
 	return 0;
 }
