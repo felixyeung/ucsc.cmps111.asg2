d41da50ffb8b7862b0652c1414c69279036024e1
Matthew Menzel Musselman <mmusselm@unix2.ic.ucsc.edu>
2012-05-11 00:53:20 -0700

    Fixed shit again cuz im boss

--- home/noStarvingWriters.c
+++ home/noStarvingWriters.c
@@ -3,31 +3,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <lockcond.h>
-#include <sema.h>
 
 lock mutex;
-int write;
+int writes;
 int numWriters=0, numReaders=0;
 
 
 void writer() {
 	int p = getpid();
 	printf("%d: writer initialized\n", p);
-	lock_acquire(mutex);
+	lock_acquire(&mutex);
 	printf("%d: writer has acquired mutex\n", p);
 	numWriters += 1;
-	semdown(write);
+	semdown(writes);
 	printf("%d: writer has grabbed the semaphore\n", p);
-	lock_release(mutex);
+	lock_release(&mutex);
 	printf("%d: writer has released mutex; now writing\n", p);
-	sleep(rand() % 200 + 20);
+	usleep(rand() % 20000 + 200);
 	printf("%d: writer is done writing\n", p);
-	lock_acquire(mutex);
+	lock_acquire(&mutex);
 	printf("%d: writer has acquired mutex\n", p);
-	semup(write);
+	semup(writes);
 	numWriters -= 1;
 	printf("%d: writer has released the semaphore\n", p);
-	lock_release(mutex);
+	lock_release(&mutex);
 	printf("%d: writer has released mutex\n", p);
 	printf("%d: writer terminated\n", p);
 }
@@ -35,25 +34,25 @@ void writer() {
 void reader() {
 	int p = getpid();
 	printf("%d: reader initialized\n", p);
-	lock_acquire(mutex);
+	lock_acquire(&mutex);
 	printf("%d: reader has acquired mutex\n", p);
-	if(numReaders == 0 || numWriteres > 0) {
-		semdown(write);
+	if(numReaders == 0 || numWriters > 0) {
+		semdown(writes);
 		printf("%d: reader has grabbed the semaphore\n", p);
 	}
 	numReaders += 1;
-	lock_release(mutex);
+	lock_release(&mutex);
 	printf("%d: reader has released mutex; now reading\n", p);
-	sleep(rand() % 200 + 20);
+	usleep(rand() % 20000 + 200);
 	printf("%d: reader is done reading\n", p);
-	lock_acquire(mutex);
+	lock_acquire(&mutex);
 	printf("%d: reader has acquired mutex\n", p);
 	numReaders -= 1;
 	if(numReaders == 0) {
-		semup(write);
+		semup(writes);
 		printf("%d: reader has released the semaphore\n", p);
 	}
-	lock_release(mutex);
+	lock_release(&mutex);
 	printf("%d: reader has released mutex\n", p);
 	printf("%d: reader terminated\n", p);
 }
@@ -63,26 +62,27 @@ int main() {
 	pid_t pid;
 	int i, numProcs = 20;
 
-	lock_init(mutex);
-	write = seminit(0, 1);
+	lock_init(&mutex);
+	writes = seminit(0, 1);
 	
 	srand(time(NULL));
 
 	for(i=0; i<numProcs; i++) {
+		
 		pid = fork();
 
-		if( pid == -1 ) {
-			printf("ERROR ON FORK!");
-			return -1;
-		}
-
 		if( pid == 0 ) {
+			printf("new child is...");
 			if(rand()%1 == 0) {
+				printf("reader!\n");
 				reader();
 			} else {
+				printf("writer!\n");
 				writer();
 			}
 		}
 	}
+	lock_release(&mutex);
+	semfree(writes);
 	printf("END NO STARVING WRITERS\n");
 }
--- usr/include/lockcond.h
+++ usr/include/lockcond.h
@@ -3,7 +3,7 @@
 #include <sys/types.h>
 #include <sema.h>
 
-typedef struct {
+typedef struct lock {
 	//this holds a semaphore identifier
 	int mutex;
 	int next;
@@ -12,56 +12,56 @@ typedef struct {
 	int nextCount;
 } lock;
 
-typedef struct {
+typedef struct cond {
 	lock *l;
 	//this is a semaphore
 	int condSem;
 	
 	//this is really an int
-	int semCount = 0;
+	int semCount;
 } cond;
 
 PUBLIC void lock_init(struct lock* l) {
-	l.mutex = seminit(0, 1);
-	l.next = seminit(0, 0);
+	(*l).mutex = seminit(0, 1);
+	(*l).next = seminit(0, 0);
 	
-	l.nextCount = 0;
+	(*l).nextCount = 0;
 }
 
 PUBLIC void lock_acquire(struct lock* l) {
-	semdown(l.mutex);
+	semdown((*l).mutex);
 }
 
 PUBLIC void lock_release(struct lock* l) {
-	if (l.nextCount > 0)
-		semup(l.next);
+	if ((*l).nextCount > 0)
+		semup((*l).next);
 	else
-		semup(l.mutex);
+		semup((*l).mutex);
 }
 
 PUBLIC void cond_init(struct cond* c, struct lock* l) {
-	lock_init(l)
-	c.l = l;
-	c.condSem = seminit(0,0);
+	lock_init(l);
+	(*c).l = l;
+	(*c).condSem = seminit(0,0);
 	
-	c.semCount = 0;
+	(*c).semCount = 0;
 }
 
 PUBLIC int cond_wait(struct cond* c) {
-	c.semCount += 1;
-	if (c.l.nextCount > 0)
-		semup(c.l.next);
+	(*c).semCount += 1;
+	if ((*c).l->nextCount > 0)
+		semup((*c).l->next);
 	else
-		semup(c.l.mutex);
-	c.condSem.down();
-	c.semCount -= 1;
+		semup((*c).l->mutex);
+	semdown((*c).condSem);
+	(*c).semCount -= 1;
 }
 
 PUBLIC int cond_signal (struct cond* c) {
-	if (c. semCount > 0) {
-		c.l.nextCount += 1;
-		c.(condSem);
-		semdown(c.l.next);
-		c.l.nextCount -= 1;
+	if ((*c).semCount > 0) {
+		(*c).l->nextCount += 1;
+		semup((*c).condSem);
+		semdown((*c).l->next);
+		(*c).l->nextCount -= 1;
 	}
 }
